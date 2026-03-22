eb774e96457009a7e06f18eea5d60f9190be1967
mertCukadar <cukadar.mertkaan@gmail.com>
2026-03-22 18:48:46 +0300

    initial_commit

---
+++ blog_page/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace blog_page.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

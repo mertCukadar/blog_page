027d2a5efe5cb71e6f383b818cd1f595e6e2b09e
mertCukadar <cukadar.mertkaan@gmail.com>
2026-03-22 21:19:45 +0300

    Auth_controller and views added

---
+++ blog_page/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace blog_page.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
